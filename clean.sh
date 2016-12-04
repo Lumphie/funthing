2fd74c169bdcc7a39edf5ab287a396fb94b41d31
Kees <wesselinkkees@gmail.com>
2016-12-04 16:14:34 +0100

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -4,8 +4,6 @@ rm Makefile
 rm Makefile.Debug
 rm Makefile.Release
 rm *.pro.user
-rm travis_qmake_gcc_cpp11_boost_test_gcov_oclint
-rm travis_qmake_gcc_cpp11_boost_test_gcov_oclint_test
 rm *.gcda
 rm *.gcno
 rm *.gcov
