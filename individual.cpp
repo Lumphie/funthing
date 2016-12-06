71cba5691802ec496506c3b1fba2bb2f55174411
Kees Wesselink <wesselinkkees@MacBook-Air-van-Kees.local>
2016-12-06 17:31:04 +0100

    added movement function body's

--- individual.cpp
+++ individual.cpp
@@ -1,9 +1,45 @@
 #include "individual.h"
+#include <stdexcept>
 
 individual::individual()
     : m_health{1.0},
       m_hunger{0.0},
       m_thirst{0.0}
+{}
+
+void individual::make_random_move(const int distance, std::mt19937& gen)
+{
+    std::uniform_int_distribution<> dis(0,4);
+    switch(dis(gen))
+    {
+    case 0: this->move_left(distance); break;
+    case 1: this->move_right(distance); break;
+    case 2: this->move_up(distance); break;
+    case 3: this->move_down(distance); break;
+    default: throw std::invalid_argument("Should not get here!");
+
+    }
+}
+
+void individual::move_down(const int distance, const int max_y)
+{
+    int y = this->get_y_coord() = distance;
+    y = y%max_y;
+    assert(y >= 10)
+    this->set_y_coord(y);
+}
+
+void individual::move_left(const int distance, const int max_x)
+{
+
+}
+
+void individual::move_right(const int distance, const int max_x)
+{
+
+}
+
+void individual::move_up(const int distance, const int max_y)
 {
 
 }
--- individual.h
+++ individual.h
@@ -1,6 +1,7 @@
 #ifndef INDIVIDUAL_H
 #define INDIVIDUAL_H
 
+#include <random>
 
 class individual
 {
@@ -10,17 +11,28 @@ public:
     double get_health() {return m_health;}
     double get_hunger() {return m_hunger;}
     double get_thirst() {return m_thirst;}
-
+    int get_x_coord() {return m_x_coord;}
+    int get_y_coord() {return m_y_coord;}
 
     void set_health(const double health) {m_health = health;}
     void set_hunger(const double hunger) {m_hunger = hunger;}
     void set_thirst(const double thirst) {m_thirst = thirst;}
-
+    void set_x_coord(const int x) {m_x_coord = x;}
+    void set_y_coord(const int y) {m_y_coord = y;}
+    void make_random_move(const int distance);
 
 private:
     double m_health;
     double m_hunger;
     double m_thirst;
+    int m_x_coord;
+    int m_y_coord;
+
+
+    void move_left(const int distance, const int max_x);
+    void move_right(const int distance, const int max_x);
+    void move_up(const int distance, const int max_y);
+    void move_down(const int distance, const int max_y);
 };
 
 #endif // INDIVIDUAL_H
